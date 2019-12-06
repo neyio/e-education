9664b9ee4d027530ad9e9de1bd3b12a9bfc21655
Neyio <neyioqian@gmail.com>
2019-12-06 13:38:33 +0800

    first initial

---
+++ support/config.js
@@ -0,0 +1,12 @@
+/* 
+  this file copy from 
+  https://github.com/ianstormtaylor/slate-plugins/tree/master/support/rollup 
+  and add some custom changes
+*/
+
+import factory from './factory';
+// import any from '../packages/any/package.json';
+// configurations = [...factory(any),... ]
+const configurations = [];
+
+export default configurations;
